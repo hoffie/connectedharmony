26f3498bd78898dc7c3823f14505c8d4508f9830
Christian Hoffmann <mail@hoffmann-christian.info>
2020-05-22 15:17:03 +0200

    warn mac/safari users

--- ui/app.js
+++ ui/app.js
@@ -639,7 +639,7 @@ const app = new Vue({
       if (!window.MediaStreamRecorder || !window.AudioContext) {
         return true;
       }
-      if (/Presto|Edge\/|Trident|iPhone|iPad/.test(window.navigator.userAgent)) {
+      if (/Presto|Edge\/|Trident|iPhone|iPad|Mac OS X.*Safari/.test(window.navigator.userAgent)) {
         return true;
       }
       return false;
